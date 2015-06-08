de36fbe81c29e37c8b0b332e98d37b2a285d2b7b
Adam McFadden & Alec Arme <student@epicodus.com>
2015-06-08 13:58:21 -0700

    Create initial content block. Update styling and containers.

--- js/router.js
+++ js/router.js
@@ -1,5 +1,8 @@
 Hodor.Router.map(function() {
-  this.resource('hodor0', {path: '/'});
+  this.resource('hodor0', {path: '/'}, function() {
+    this.resource('content0');
+    this.resource('content1');
+  });
   this.resource('hodor1');
   this.resource('hodor2');
 });
